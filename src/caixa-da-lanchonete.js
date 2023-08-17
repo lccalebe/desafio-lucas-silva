b6ff26f6ddf75fe03729e9955435a58108b7362b
Lucas <lukscalebe@hotmail.com>
2023-08-16 21:47:05 -0300

    atualizando

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,7 +1,22 @@
 class CaixaDaLanchonete {
 
     calcularValorDaCompra(metodoDePagamento, itens) {
-        return "";
+
+        itensTam = itens.lenght;
+
+        if (itensTam == 0) {
+            return "Não há itens no carrinho de compra!"
+        }else {
+            if (metodoDePagamento == 'dinheiro') {
+                
+            } else if (metodoDePagamento == 'debito') {
+                
+            } else if (metodoDePagamento == 'credito') {
+                
+            } else {
+                return "Forma de pagamento inválida!";
+            };
+        };
     }
 
 }
